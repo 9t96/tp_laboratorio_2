bd6b632f6b40f5b174ffe63838dd287ccd995649
claudio mazanares <claudio mazanares>
2016-11-28 18:58:50 -0300

    Tp4 Subido y terminado.

---
+++ Manzanares.Claudio.Manuel.2ºA.TP4/Archivos/Texto.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Archivos
+{
+    public class Texto : IArchivo<string>
+    {
+        string ruta;
+        
+        /// <summary>
+        /// Asigana la ruta del archivo.
+        /// </summary>
+        /// <param name="ruta"></param>
+        public Texto(string ruta)
+        {
+            this.ruta = ruta;
+        }
+
+        /// <summary>
+        /// Guarda los datos pasados por parametro en un archivo de texto.
+        /// </summary>
+        /// <param name="datos"></param>
+        /// <returns></returns>
+        public bool guardar(string datos)
+        {
+            bool succed = false;
+
+            try
+            {
+                using (StreamWriter escritor = new StreamWriter(ruta,true))
+                {
+                    escritor.WriteLine(datos);
+                    succed = true;
+                }
+            }
+            catch (Exception e)
+            {
+                succed = false;
+                throw e;
+            }
+
+            return succed;
+        }
+
+        /// <summary>
+        /// Lee los datos y los guarda en una lista generica de string.
+        /// </summary>
+        /// <param name="datos"></param>
+        /// <returns></returns>
+        public bool leer(out List<string> datos)
+        {
+            bool succed = false;
+            datos = new List<string>();
+
+            try
+            {
+                using (StreamReader lector = new StreamReader(ruta))
+                {
+                    while (!lector.EndOfStream)
+                    {
+                        datos.Add(lector.ReadLine());
+                    }
+
+                    succed = true;
+                }
+            }
+            catch (Exception)
+            {
+                succed = false;
+                datos = default(List<string>);
+            }
+
+            return succed;
+        }
+    }
+}
---
+++ Manzanares.Claudio.Manuel.2ºA.TP4/Hilo/Descargador.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Net; // Avisar del espacio de nombre
+using System.ComponentModel;
+
+namespace Hilo
+{
+    public class Descargador
+    {
+        private string html;
+        private Uri direccion;
+
+        public Descargador(Uri direccion)
+        {
+            this.direccion = direccion;
+        }
+
+        public void IniciarDescarga()
+        {
+            try
+            {
+                WebClient cliente = new WebClient();
+                cliente.DownloadProgressChanged += WebClientDownloadProgressChanged;
+                cliente.DownloadStringCompleted += WebClientDownloadCompleted;
+
+                cliente.DownloadStringAsync(this.direccion);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public delegate void FinDescargaEventHandler(string html);
+        public event FinDescargaEventHandler DescargaFinalizada;
+
+        private void WebClientDownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            if (ProgresoDescarga != null)
+                ProgresoDescarga(e.ProgressPercentage);
+        }
+
+        public delegate void ProgresoDescargaEventHandler(int progreso);
+        public event ProgresoDescargaEventHandler ProgresoDescarga;
+
+        private void WebClientDownloadCompleted(object sender, DownloadStringCompletedEventArgs e)
+        {
+            try
+            {
+
+                this.html = e.Result;
+            }
+            catch (Exception ex)
+            {
+
+                this.html = ex.InnerException.Message;
+            }
+            finally
+            {
+
+                this.DescargaFinalizada(this.html);
+            }
+        }
+    }
+}
